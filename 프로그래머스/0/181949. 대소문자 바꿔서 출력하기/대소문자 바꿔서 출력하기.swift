be1dd031c397678b17ef92c5399fadf518b1364a
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-10-27 11:07:00 +0900

    Update 대소문자 바꿔서 출력하기.swift
    add other answer

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
@@ -11,3 +11,21 @@ for i in s1 {
     }
 }
 print(str)
+
+/*
+1) 삼항연산자로 각각 바꿔서 맴핑 후 합혀서 출력
+print(readLine()!.map { $0.isLowercase ? $0.uppercased() : $0.lowercased() }.joined())
+
+2) 
+for s in Array(s1) {
+    answer += s.isUppercase ? s.lowercased() : s.uppercased()
+}
+
+3)
+var result = String()
+for s in s1 {
+    if s.isUppercase { result.append(s.lowercased()) }
+    else { result.append(s.uppercased()) }
+}
+
+*/
