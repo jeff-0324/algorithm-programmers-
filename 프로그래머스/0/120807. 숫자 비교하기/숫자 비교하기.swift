84a0d1cdd923e49ec95c7969cd85e4b22acfef3d
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-02 20:16:26 +0900

    [comment]  : other solution

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -11,3 +11,17 @@ func solution(_ num1:Int, _ num2:Int) -> Int {
     }
     return result
 }
+
+/*
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard (num1 >= 0 && num1 <= 10000) , (num2 >= 0 && num2 <= 10000) else {
+        return 0
+    }
+    return num1 == num2 ? 1 : -1
+}
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard 0...10000 ~= num1, 0...10000 ~= num2 else { return 0 }
+    return num1 == num2 ? 1 : -1
+}
+*/
