f9841311e131e6f3b5a864700983c031c0bf5731
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-04 09:11:50 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard (0 < num1 && num1 <= 100) && (0 < num2 && num2 <= 100) else { return -1 }
+    if num2 == 0 {
+        return -1
+    } else {
+        return Int(Double(num1) / Double(num2) * 1000.0)
+    }
+}
