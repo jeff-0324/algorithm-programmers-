8bdf57bde35f91acc54018eafe07a3e384b7ce76
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-07 10:21:47 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
@@ -1,21 +1,16 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Double {
-    var sum = 0
-    var avg = 0.0
+    var sum: Double = 0.0
+    var avg: Double = 0.0
     
     guard 1 <= numbers.count && numbers.count <= 100 else { return 0}
     for i in numbers {
         if 0 <= i && i <= 1000 {
-            sum += i
+            sum += Double(i)
         }
     }
     
     avg = Double(sum) / Double(numbers.count)
-    let avgCheck = avg - floor(avg)
-    if  avgCheck == 0.0 || avgCheck == 0.5 {
-        return avg
-    } else {
-        return Double(Int(avg * 2 + 1)) / 2.0
-    }
+    return avg
 }
