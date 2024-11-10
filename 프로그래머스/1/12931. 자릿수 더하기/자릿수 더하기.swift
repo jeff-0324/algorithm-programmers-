6a2ecaf7404aeb4a5b0157a0dadd93d2622bff79
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-10 18:38:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -10,3 +10,26 @@ func solution(_ n:Int) -> Int
     answer = charArray.compactMap { Int(String($0)) }.reduce(0, +)
     return answer
 }
+
+/* other solution 
+1. 
+func solution(_ n:Int) -> Int
+{
+    return String(n).reduce(0, {$0+Int(String($1))!});
+}
+
+2.
+func solution(_ n:Int) -> Int
+{
+    var answer:Int = 0
+    var val = n
+
+    while val > 0 {
+        answer += val % 10
+        val /= 10
+    }
+
+    return answer
+}
+
+*/
