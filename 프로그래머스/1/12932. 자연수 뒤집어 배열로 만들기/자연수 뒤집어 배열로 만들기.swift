2a5bbbf6ef6906a49705c6b28dc700abae3afe41
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-14 09:55:00 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.15 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
@@ -3,6 +3,19 @@ func solution(_ n: Int64) -> [Int] {
     if n <= 10000000000 {
         let charArray = Array(String(n).reversed())
         result = charArray.compactMap { $0.wholeNumberValue }
-    }
+    } 
     return result
 }
+
+/* other solution
+func solution(_ n:Int64) -> [Int] {
+    var num: Int = Int(n)
+    var arr: [Int] = []
+
+    while num > 0 {
+        arr.append(num % 10)
+        num /= 10
+    }
+    return arr
+}
+*/
