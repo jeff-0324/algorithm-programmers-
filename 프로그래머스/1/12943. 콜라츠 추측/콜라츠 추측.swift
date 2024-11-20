e4172ec0e871f2dd04fb1cd93845d171d1f27c0d
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-20 22:27:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -15,3 +15,25 @@ func solution(_ num:Int) -> Int {
     }
     return count
 }
+
+/* other solution
+
+func odd(_ n: Int) -> Int {
+    return n * 3 + 1
+}
+
+func even(_ n: Int) -> Int {
+    return n / 2
+}
+
+func solution(_ num:Int) -> Int {
+    var ans = num
+    var count = 0
+    while ans != 1 && count < 500 {
+        ans = ans % 2 == 0 ? even(ans) : odd(ans)
+        count += 1
+    }
+    return count >= 500 ? -1 : count
+}
+
+*/
