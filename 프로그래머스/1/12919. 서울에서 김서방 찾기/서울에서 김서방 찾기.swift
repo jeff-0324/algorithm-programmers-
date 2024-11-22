e311e516948fd26b9ff7b3d77dc407dc26a6a73e
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-11-22 11:31:33 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -0,0 +1,10 @@
+func solution(_ seoul:[String]) -> String {
+    var count = 0
+    for i in seoul {
+        count += 1
+        if i == "Kim" {
+            break
+        }
+    }
+    return "김서방은 \(count - 1)에 있다"
+}
