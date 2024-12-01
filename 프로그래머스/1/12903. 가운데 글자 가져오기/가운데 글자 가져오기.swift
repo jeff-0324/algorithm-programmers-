6757536559b1e58da545257191978616edaca659
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-12-01 22:22:10 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -7,3 +7,13 @@ func solution(_ s:String) -> String {
         return "\(result[centerIndex - 1])" + "\(result[centerIndex])"
     }
 }
+
+/*
+func solution(_ s:String) -> String {
+    if Array(s).count % 2 == 0 {
+        return String(Array(s)[(s.count/2)-1...(s.count/2)])
+    }else{
+        return String(Array(s)[s.count/2])
+    }
+}
+*/
