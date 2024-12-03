f4402945eecd4afd362e7d90ca3cb27cdc6259de
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-12-03 20:12:42 +0900

    [level 1] Title: 내적, Time: 0.82 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,10 +1,17 @@
 import Foundation
 
+/*
 func solution(_ a:[Int], _ b:[Int]) -> Int {
     var result = 0
     for i in 0..<a.count {
         var multiple = a[i]*b[i]
         result += multiple
     }
+    return result
+}
+*/
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    var result = zip(a,b).map { $0*$1 }.reduce(0 , +)
     return result
 }
