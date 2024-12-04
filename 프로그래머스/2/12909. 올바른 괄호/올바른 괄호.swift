5d6a2f7d0049cc9ce21cc4f573f6ac3cc34c39fc
jeff-0324 <142293514+jeff-0324@users.noreply.github.com>
2024-12-04 11:30:33 +0900

    [level 2] Title: 올바른 괄호, Time: 10.83 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -1,15 +1,11 @@
-func solution(_ s:String) -> Bool {
-    var stack:[Character] = []
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var ans:Bool = false
     
-    for char in s {
-        if char == "(" {
-            stack.append(char)
-        } else if char == ")" {
-            if stack.isEmpty {
-                return false
-            }
-            stack.popLast()
-        }
-    }
-    return stack.isEmpty
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return ans
 }
